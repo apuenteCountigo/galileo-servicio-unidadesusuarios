9988f710de63119646efadbb20d0e277a31f473c
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-08-21 15:30:09 -0400

    solucionando conflicto entre repositorios

--- servicio-unidades-usuarios/src/main/java/com/galileo/cu/unidadesusuarios/repositorios/ExpiraUserRepository.java
+++ servicio-unidades-usuarios/src/main/java/com/galileo/cu/unidadesusuarios/repositorios/ExpiraUserRepository.java
@@ -1,6 +1,7 @@
 package com.galileo.cu.unidadesusuarios.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.galileo.cu.commons.models.UnidadesUsuarios;
@@ -8,6 +9,7 @@ import com.galileo.cu.commons.models.UnidadesUsuarios;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@RepositoryRestResource(exported = false)
 public interface ExpiraUserRepository extends JpaRepository<UnidadesUsuarios, Long> {
     List<UnidadesUsuarios> findByExpiraBefore(LocalDateTime dateTime);
 }
