43c605e64d1255c825164c34f68eb45a4660c051
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-08-23 00:31:55 -0400

    @RepositoryRestResource(exported = false)

--- servicio-unidades-usuarios/src/main/java/com/galileo/cu/unidadesusuarios/repositorios/ConexionesRepository.java
+++ servicio-unidades-usuarios/src/main/java/com/galileo/cu/unidadesusuarios/repositorios/ConexionesRepository.java
@@ -1,10 +1,12 @@
 package com.galileo.cu.unidadesusuarios.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.galileo.cu.commons.models.Conexiones;
 import java.util.List;
 
+@RepositoryRestResource(exported = false)
 public interface ConexionesRepository extends JpaRepository<Conexiones, Long> {
     List<Conexiones> findByServicio(String servicio);
 }
