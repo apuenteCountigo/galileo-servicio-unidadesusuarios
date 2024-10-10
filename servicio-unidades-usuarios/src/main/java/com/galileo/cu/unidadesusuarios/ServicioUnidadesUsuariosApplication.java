0db46d79fff6d3d999e3fb47af5c1671e9ac889b
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-10 12:56:51 -0400

    Recompilando

--- servicio-unidades-usuarios/src/main/java/com/galileo/cu/unidadesusuarios/ServicioUnidadesUsuariosApplication.java
+++ servicio-unidades-usuarios/src/main/java/com/galileo/cu/unidadesusuarios/ServicioUnidadesUsuariosApplication.java
@@ -22,7 +22,7 @@ public class ServicioUnidadesUsuariosApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("Unidades Usuarios V-24-10-10 12:52");
+		System.out.println("Unidades Usuarios V-24-10-10 12:56");
 	}
 
 }
