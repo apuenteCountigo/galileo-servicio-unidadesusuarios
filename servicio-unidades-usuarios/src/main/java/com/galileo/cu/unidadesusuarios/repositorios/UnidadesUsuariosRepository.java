7d0b6c928e9665ca6b5e8dd03fe463e194c996e3
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-10 16:19:36 -0400

    Refactorizando querys in the repository

--- servicio-unidades-usuarios/src/main/java/com/galileo/cu/unidadesusuarios/repositorios/UnidadesUsuariosRepository.java
+++ servicio-unidades-usuarios/src/main/java/com/galileo/cu/unidadesusuarios/repositorios/UnidadesUsuariosRepository.java
@@ -1,51 +1,54 @@
 package com.galileo.cu.unidadesusuarios.repositorios;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
 
 import com.galileo.cu.commons.models.UnidadesUsuarios;
 
 @CrossOrigin(origins = "*")
 @RepositoryRestResource(collectionResourceRel = "unidadesUsuarios", path = "listar")
 public interface UnidadesUsuariosRepository extends PagingAndSortingRepository<UnidadesUsuarios, Long> {
-	
-	@Query("SELECT u FROM UnidadesUsuarios u WHERE (:idUnidad=0 or u.unidad.id = :idUnidad) "
-			+"AND (:idEstado=0 or u.estado.Id=:idEstado) "
-			+ "AND (:nombre='' or u.usuario.nombre like %:nombre%) "
-			+ "AND (:apellidos='' or u.usuario.apellidos like %:apellidos%) " 
-			+ "AND (:tip='' or u.usuario.tip like %:tip%) "
-			+ " AND ( "
-			+ "			(:idAuth IN (SELECT up FROM Usuarios up WHERE up.perfil.id=1)) "
-			+ "	OR ( "
-			+ "		(:idAuth IN (SELECT up FROM Usuarios up WHERE up.perfil.id=2)) "
-			+ "		AND (u.unidad.id=(SELECT unidad.id FROM UnidadesUsuarios WHERE usuario.id=:idAuth AND estado.id=6)) "
-			+ "		) "
+
+	@Query("SELECT u FROM UnidadesUsuarios u WHERE (:idUnidad = 0 OR u.unidad.id = :idUnidad) "
+			+ "AND (:idEstado = 0 OR u.estado.Id = :idEstado) "
+			+ "AND (:nombre = '' OR u.usuario.nombre LIKE CONCAT('%', :nombre, '%')) "
+			+ "AND (:apellidos = '' OR u.usuario.apellidos LIKE CONCAT('%', :apellidos, '%')) "
+			+ "AND (:tip = '' OR u.usuario.tip LIKE CONCAT('%', :tip, '%')) "
+			+ "AND ( "
+			+ "     (:idAuth IN (SELECT up.id FROM Usuarios up WHERE up.perfil.id = 1)) "
+			+ "  OR ( "
+			+ "     (:idAuth IN (SELECT up.id FROM Usuarios up WHERE up.perfil.id = 2)) "
+			+ "     AND (u.unidad.id = (SELECT uu.unidad.id FROM UnidadesUsuarios uu WHERE uu.usuario.id = :idAuth AND uu.estado.id = 6)) "
+			+ "     ) "
 			+ ") "
-			+ "and ((:fechaFin!=null and :fechaInicio!=null and u.expira between :fechaInicio and :fechaFin) "
-			+ "or (:fechaFin=null and :fechaInicio!=null and u.expira >=:fechaInicio) "
-			+ "or (:fechaFin=null and :fechaInicio=null)) ")
-	public Page<UnidadesUsuarios> filtrarUsuarios(long idAuth,int idUnidad,int idEstado,String tip, String nombre, String apellidos,@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaInicio, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaFin, Pageable p);
-	
-	@Query("SELECT u FROM UnidadesUsuarios u WHERE (:idUsuario=0 or u.usuario.id = :idUsuario) "
-			+"AND (:idEstado=0 or u.estado.Id=:idEstado) "
-			+ " AND ( "
-			+ "			(:idAuth IN (SELECT up FROM Usuarios up WHERE up.perfil.id=1)) "
-			+ "	OR ( "
-			+ "		(:idAuth IN (SELECT up FROM Usuarios up WHERE up.perfil.id=2)) "
-			+ "		AND (u.unidad.id=(SELECT unidad.id FROM UnidadesUsuarios WHERE usuario.id=:idAuth AND estado.id=6)) "
-			+ "		) "
+			+ "AND ( "
+			+ "     (:fechaFin IS NOT NULL AND :fechaInicio IS NOT NULL AND u.expira BETWEEN :fechaInicio AND :fechaFin) "
+			+ "  OR (:fechaFin IS NULL AND :fechaInicio IS NOT NULL AND u.expira >= :fechaInicio) "
+			+ "  OR (:fechaFin IS NULL AND :fechaInicio IS NULL) "
+			+ ") ")
+	public Page<UnidadesUsuarios> filtrarUsuarios(long idAuth, int idUnidad, int idEstado, String tip,
+			String nombre, String apellidos, LocalDateTime fechaInicio, LocalDateTime fechaFin, Pageable p);
+
+	@Query("SELECT u FROM UnidadesUsuarios u WHERE (:idUsuario = 0 OR u.usuario.id = :idUsuario) "
+			+ "AND (:idEstado = 0 OR u.estado.Id = :idEstado) "
+			+ "AND ( "
+			+ "     (:idAuth IN (SELECT up.id FROM Usuarios up WHERE up.perfil.id = 1)) "
+			+ "  OR ( "
+			+ "     (:idAuth IN (SELECT up.id FROM Usuarios up WHERE up.perfil.id = 2)) "
+			+ "     AND (u.unidad.id = (SELECT uu.unidad.id FROM UnidadesUsuarios uu WHERE uu.usuario.id = :idAuth AND uu.estado.id = 6)) "
+			+ "     ) "
 			+ ") "
-			+ "and ((:fechaFin!=null and :fechaInicio!=null and u.expira between :fechaInicio and :fechaFin) "
-			+ "or (:fechaFin=null and :fechaInicio!=null and u.expira >=:fechaInicio) "
-			+ "or (:fechaFin=null and :fechaInicio=null)) ")
-	public Page<UnidadesUsuarios> filtrarUnidades(long idAuth,int idUsuario,int idEstado,@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaInicio, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaFin, Pageable p);
+			+ "AND ( "
+			+ "     (:fechaFin IS NOT NULL AND :fechaInicio IS NOT NULL AND u.expira BETWEEN :fechaInicio AND :fechaFin) "
+			+ "  OR (:fechaFin IS NULL AND :fechaInicio IS NOT NULL AND u.expira >= :fechaInicio) "
+			+ "  OR (:fechaFin IS NULL AND :fechaInicio IS NULL) "
+			+ ") ")
+	public Page<UnidadesUsuarios> filtrarUnidades(long idAuth, int idUsuario, int idEstado,
+			LocalDateTime fechaInicio, LocalDateTime fechaFin, Pageable p);
 }
